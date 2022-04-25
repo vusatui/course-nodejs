c1a2a92a3c9e17f493a899b4d64528283a94efc8
Anton_Vusatyi <Anton_Vusatyi@vfc.com>
2022-04-25 21:28:29 +0300

    task-4 Cherry-picked changes.

--- src/loaders/expressLoader.ts
+++ src/loaders/expressLoader.ts
@@ -1,24 +1,18 @@
-import { MicroframeworkSettings } from "microframework";
 import express from "express";
 
 import config from "../config";
 import apiV1 from "../api";
 
 
-export const expressLoader = (settings: MicroframeworkSettings) => {
+export const expressLoader = () => {
     const app = express();
 
     app.use(express.json());
     app.use(config.api.v1.prefix, apiV1());
 
-    app.all("/status", (req, res) => {
-        res.end("ok");
-    });
+    app.all("/status",
+        (req, res) => res.end("ok"));
 
-    app.listen(config.expressPort, () => {
-        console.log(`Server listening on port: ${config.expressPort}`);
-    }).on("error", (err) => {
-        console.error(err.message);
-        process.exit(1);
-    });
+    app.listen(config.expressPort, () => console.log(`Server listening on port: ${config.expressPort}`))
+        .on("error", (err) => console.error(err.message));
 };
